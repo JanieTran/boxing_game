f89731b9c408594b58512282043e6959720b8fe3
JanieTran <emerald1901@gmail.com>
2017-05-17 17:56:29 +0700

    Comment

--- boxing_game.cpp
+++ boxing_game.cpp
@@ -1,7 +1,18 @@
-// Name        : boxing_game.cpp
-// Author      : Pan
-// Version     : 24 April 2017
-// Description : This is a simple motion tracker that is packaged in a class
+//============================================================================
+// Name        : EEET2448 COMPUTING ENGINEERING 2017A - ASSIGNMENT 4
+// Author      : - Bui Khac Phuong Uyen _ s3618720 : ROI seperation and health system
+//				 - Tran Thi Hong Phuong _ s3623386 : Color detection and special effects
+// Copyright   : Your copyright notice
+// Description : This is an interactive 2-player boxing game. With 2 cameras for 2 players, the program detects motion using
+//	color tracking and allows players to control their representative figure displayed on screen. Each time a player is hit,
+//	his health decreases. The game ended when either player runs out of health.
+//
+// During the process, we encountered some difficulties such as:
+// 		- Setting conditions so that no ROI overlaps one another.
+//		- Using unique colour for best ROI performance.
+//		- Have to use 2 cameras.
+// Reference	: EEET2448 COMPUTING ENGINEERING Week 8 & 10 codes - Mr. Vlad Mariano
+//============================================================================
 
 #include <cv.h>   // all the OpenCV headers, which links to all the libraries
 #include <highgui.h>
@@ -17,7 +28,8 @@
 using namespace std;
 using namespace cv;   // a "shortcut" for directly using OpenCV functions
 
-class ScreenObs {// This is the parent class of all objects on screen
+	// Parent class of all objects
+class ScreenObs {
 public:
 	// Attribute
 	Scalar obColour;
@@ -30,36 +42,36 @@ public:
 		obColour = set_obColour;
 	}
 };
+
+	// Class for Motion Trackers
 class moTracker: public ScreenObs {
 public:
 	// ROI Attributes
-	Mat prePrev, prevFrame, curFrame; //saved frames for finding differences
-	//includes 2nd preivous frame, previous frame, and current frame
-	int xROI, yROI, wROI, hROI; // set ROI's dimensions
-	int xCOM, yCOM; //coordinates of Centre of Mass
-	bool firstRun; //check whether it is the first time the programme runs
+	int xROI, yROI, wROI, hROI; 	// ROI's dimensions
+	int xCOM, yCOM; 		// coordinates of Centre of Mass
+	bool firstRun; 			// check whether it is the first time the programme runs
 
-	int topLim, leftLim, botLim, rLim; // ROI can only move within this region
-	bool limSet; // are specific limits set for ROI?
+	int topLim, leftLim, botLim, rLim; 	// ROI can only move within this region
+	bool limSet; 				// are specific limits set for ROI?
 
 	// Attributes of a player
-	int xHead, yHead, headRad; //head set
-	int xlHand, ylHand, handRad, xrHand, yrHand; //hand set
-	int p2Factor; // applied in calculation depends on which player is using function
-	bool player2; // check if the ROI belongs to player 2
+	int xHead, yHead, headRad; 			// head set
+	int xlHand, ylHand, handRad, xrHand, yrHand; 	// hand set
+	int p2Factor; 					// applied in calculation depends on which player is using function
+	bool player2; 					// check if the ROI belongs to player 2
 	bool isTouching, hit;
 	int hitNo;
 	int xlEye;
 	int xrEye;
 	int yEye;
 
-	//Stamina bar attributes
+	// Stamina bar attributes
 	int xBar, yBar, wBar, hBar, maxStat;
 	int xBox, yBox, wBox, hBox;
 	int countFrame;
 
 	moTracker () {
-		//default setROI
+		// default setROI
 		xROI = 100;
 		yROI = 200;
 		wROI = 50;
@@ -69,8 +81,8 @@ public:
 		maxStat = 160;
 
 		//loop check
-		firstRun = true; //by default loop has to go through first run
-		limSet = false;  //by default no limits are set
+		firstRun = true; // by default loop has to go through first run
+		limSet = false;  // by default no limits are set
 		p2Factor = 1;
 		player2 = false;
 		isTouching = false;
@@ -79,16 +91,19 @@ public:
 		countFrame = 0;
 	}
 	void feedNewframe (Mat frame, Scalar darker, Scalar brighter) {
+		// Mat object for color detection
 		Mat colorOutput;
 
-		int x, y; //coordinates of pixel
-		//brightness of each pixel - on x,y-axis and the sum
+		int x, y; // coordinates of pixel
+		
+		// brightness of each pixel - on x,y-axis and the sum
 		int xMass, yMass, sMass, m;
 
-		if (firstRun) { //if this is the 1st Run!
+		if (firstRun) { // if this is the 1st Run!
 			xCOM = xROI + wROI/2;
 			yCOM = yROI + hROI/2;
-		// Set up stamina bar
+			
+			// Set up stamina bar
 			if (player2) {
 				xBar = (int)(frame.cols*1/16);
 				yBar = (int)(frame.rows*1/16);
@@ -103,7 +118,9 @@ public:
 			hBox = hBar + 10;
 			firstRun = false; // 1st run is over
 		}
-
+		
+		// Color detection function that detects only the color between the "darker" and "brighter" threshold,
+		// on "colorOutput" Mat, the specific color becomes white, others becomes black background
 		inRange(frame, darker, brighter, colorOutput);
 
 	// Compute COM
@@ -121,6 +138,8 @@ public:
 			yCOM = yMass/sMass;
 		}
 	}
+	
+	// Conditions for ROI location
 	void updateROI (Mat frame) {
 		xROI = xCOM - wROI/2;
 		yROI = yCOM - hROI/2;
@@ -146,11 +165,13 @@ public:
 				yROI = botLim - hROI;
 			}
 	}
+	
+	// Draw ROI on frame
 	void drawROI (Mat frame) {
 		rectangle (frame, Rect(xROI, yROI, wROI, hROI), obColour, 2);
 	}
 
-	// set ROI accordingly
+	// Customise ROI attributes
 	void setROI (int set_xROI, int set_yROI, int set_wROI,
 			int set_hROI) {
 		xROI = set_xROI;
@@ -159,7 +180,7 @@ public:
 		hROI = set_hROI;
 	}
 
-	//set limits accordingly
+	// Customise ROI boundaries
 	void setLim (int set_topLim, int set_leftLim, int set_botLim, int set_rLim) {
 		topLim = set_topLim;
 		leftLim = set_leftLim;
@@ -167,6 +188,8 @@ public:
 		rLim = set_rLim;
 		limSet = true;
 	}
+	
+	// Keep ROIs not overlap one another
 	void separateROI (moTracker t1, int r1, int r2) {
 		// keeps 2 ROIs on screen from overlapping one another
 		// t1 has the coordinates and radius of reference ROI
@@ -189,6 +212,7 @@ public:
 			yCOM = (int) (t1.yCOM + sin(alpha)*rDist);
 		}
 	}
+	
 	// Behaviours of a player
 	void drawPlayer (Mat frame, moTracker leftFist, moTracker rFist) {
 		int eyeRad = (int) headRad/10;
@@ -196,6 +220,7 @@ public:
 		headRad = (int)(wROI/2);
 		handRad = (int)(rFist.wROI/2);
 
+		// Player 2 is drawn upside down
 		if (player2) {
 			xHead = frame.cols - xCOM;
 			yHead = frame.rows - yCOM;
@@ -229,27 +254,36 @@ public:
 		circle (frame, Point(xlHand, ylHand), handRad, obColour, -1);
 		circle (frame, Point(xrHand, yrHand), handRad, obColour, -1);
 				// Eyes
+		// If being hit, eyes change in to X shape
 		if (hit == true) {
 			line(frame, Point(xlEye - 10, yEye - 10), Point(xlEye + 10, yEye + 10), Scalar(0,0,0), 4);
 			line(frame, Point(xlEye + 10, yEye - 10), Point(xlEye - 10, yEye + 10), Scalar(0,0,0), 4);
 
 			line(frame, Point(xrEye - 10, yEye - 10), Point(xrEye + 10, yEye + 10), Scalar(0,0,0), 4);
 			line(frame, Point(xrEye + 10, yEye - 10), Point(xrEye - 10, yEye + 10), Scalar(0,0,0), 4);
-		} else {
+		}
+		
+		// If not hit, eyes are black circles
+		else {
 			circle(frame, Point(xlEye, yEye), eyeRad, Scalar(0,0,0), -1);
 			circle(frame, Point(xrEye, yEye), eyeRad, Scalar(0,0,0), -1);
 		}
+		
 				// Smile
 		int xSmile = xHead;
 		int ySmile = yHead;
 
+		// If being hit, mouth changes into a black circle
 		if (hit == true) {
 			if (player2) {
 				circle(frame, Point(xHead, yHead - 20), 20, Scalar(0,0,0), -1);
 			} else {
 				circle(frame, Point(xHead, yHead + 40), 20, Scalar(0,0,0), -1);
 			}
-		} else {
+		}
+		
+		// If not hit, smile !!
+		else {
 			if (player2) {
 				ellipse(frame, Point(xSmile, ySmile), Size(headRad/2, headRad/2), 180, 0, 180, Scalar(0,0,0), 4, 8);
 			} else {
@@ -258,24 +292,29 @@ public:
 		}
 
 	}
+	
+	// Set factors for player 2
 	void p2 () {
 		p2Factor = -1;
 		player2 = true;
 	}
-	void separatePlayers (Mat frame, moTracker t1, int r1, int r2, bool Hit) { // Hit
-		// keeps 2 players from overlapping one another
+	
+	// Keep 2 players from overlapping one another
+	void separatePlayers (Mat frame, moTracker t1, int r1, int r2, bool Hit) { 
 		// t1 has the coordinates and radius of reference player
-		//the ROI using this function will have its position changed
+		// the ROI using this function will have its position changed
+		
 		int rDist = 0;
 		double delX = 0.0, delY = 0.0;
-		double alpha = 0.0; // This is the angle between the second object and
-				// the horizon of the reference object (range from 0 to pi)
+		double alpha = 0.0; 	// This is the angle between the second object and
+					// the horizon of the reference object (range from 0 to pi)
 		delX = p2Factor*((xCOM + t1.xCOM) - frame.cols);
 		delY = p2Factor*((yCOM + t1.yCOM) - frame.rows);
-		rDist = r1 + r2; // Magnitude of total radius distance between objects
-			// Calculate the distance between two object's centres
+		rDist = r1 + r2; 	// Magnitude of total radius distance between objects
+					// Calculate the distance between two object's centres
 		int dist = round(sqrt(pow(delX, 2.0) + pow (delY, 2.0)));
 
+		// If any 2 ROIs meet each other, they bounces back in the same direction in which they come in
 		if (dist < rDist) {
 			alpha = acos ((double) (delX/dist));
 			if (delY < 0) {
@@ -284,39 +323,50 @@ public:
 			xCOM = (int) (frame.cols - t1.xCOM + p2Factor*cos(alpha)*rDist);
 			yCOM = (int) (frame.rows - t1.yCOM + p2Factor*sin(alpha)*rDist);
 		}
+		
+		// Decide when fist is touching head
 		if (dist > rDist - 10 && dist < rDist + 10 && Hit == true){
 			isTouching = true;
 		}
+		
+		// Decide when fist parts from head
 		else if (dist > (rDist + 30) && Hit == true) {
 			isTouching = false;
 		}
 	}
+	
+	// Stamina and health bar
 	void stamina (Mat frame, moTracker LFist, moTracker rFist) {
 		// This function is only used by head motion tracker of each player
 
+		// If fist and head are touching, it counts as a hit
 		if (LFist.isTouching || rFist.isTouching) {
 			if (!hit && hitNo < 5) {
 				hitNo ++;
 				hit = true;
 			}
 		} else if (!LFist.isTouching && !rFist.isTouching) { hit = false; }
+		
+		// If not being hit for a period of time, the player recovers health
 		if (!hit) {
 			countFrame++;
 			if (countFrame % 100 == 0 && hitNo > 0) {
 				hitNo--;
 			}
 		} else countFrame = 0;
-				// Visualise stamina bar
+		
+		// Visualise stamina bar
 		wBar = (5 - hitNo)*maxStat/5;
 
 		if (!player2) {
 			xBar = (int)(frame.cols*11/16) + hitNo/5*maxStat;
 		}
-		// representation of stamina
+		
+		// Representation of stamina
 		rectangle (frame, Rect(xBar, yBar, wBar, hBar),
 									Scalar (0,0,255), -1);
 
-		// representation of stamina's case
+		// Representation of stamina's border case
 		rectangle (frame, Rect(xBox, yBox, wBox, hBox),
 									Scalar (255,255,255), 2);
 
@@ -330,13 +380,16 @@ int main(  int argc, char** argv ) {
 	Mat frame;
 	Mat frame2;
 
+	// File to keep record of winners
 	FILE *history;
 
 	string winner;
 
+	// Players' name
 	char player1[10];
 	char player2[10];
 
+	// Prompt the players to enter their name before starting the game
 	do {
 		printf("Enter name for Player 1: \n");
 		scanf("%s", player1);
@@ -347,28 +400,29 @@ int main(  int argc, char** argv ) {
 		scanf("%s", player2);
 	} while (player2 == NULL);
 
+	// Convert players' name to string type in order to use putText function
 	string player1Str = player1;
 	string player2Str = player2;
 
-	VideoCapture cap(0); // open the default camera
+	VideoCapture cap(0); 	// open the default camera
 	if (!cap.isOpened()) {  // check if we succeeded
 		return -1;
 	}
-	VideoCapture cap2(1); // open the default camera
+	VideoCapture cap2(1); 	// open the default camera
 	if (!cap2.isOpened()) {  // check if we succeeded
 		return -1;
 	}
 	cap  >> frame;
 	cap2 >> frame2;
 
-			// Declare players
+	// Declare players
 	Mat game = Mat (frame.rows, frame.cols, CV_8UC3);
 	Mat game2 = Mat (frame.rows, frame.cols, CV_8UC3);
 	moTracker p1, p1LHand, p1RHand;
 	moTracker p2, p2LHand, p2RHand;
 	p2.p2(); p2LHand.p2(); p2RHand.p2();
 
-			// Setup players
+	// Setup players
 	p1.setROI (frame.cols/2 - 100, 200, 150, 150);
 	p1.setColour(Scalar(255,0,0));
 	p1.setLim (frame.rows/2, (int)(frame.cols*1/7), frame.rows,
@@ -393,7 +447,7 @@ int main(  int argc, char** argv ) {
 		cap >> frame; // get a new frame from camera
 		cap2 >> frame2;
 
-				//Calculate COM and feed each frame captured
+		//Calculate COM and feed each frame captured
 		p1.feedNewframe(frame, Scalar(10,10,194), Scalar(125,125,249));
 		p1LHand.feedNewframe(frame, Scalar(0,164,164), Scalar(125,255,255));
 		p1RHand.feedNewframe(frame, Scalar(0,164,164), Scalar(125,255,255));
@@ -402,7 +456,7 @@ int main(  int argc, char** argv ) {
 		p2LHand.feedNewframe(frame2, Scalar(0,164,164), Scalar(125,255,255));
 		p2RHand.feedNewframe(frame2, Scalar(0,164,164), Scalar(125,255,255));
 
-				// Separate the ROIs of one player
+		// Separate the ROIs of one player
 		p1LHand.separateROI (p1, p1.headRad, p1.handRad);
 		p1RHand.separateROI (p1, p1.headRad, p1.handRad);
 		if (p1.xlHand + p1.handRad < p1.xHead) {
@@ -419,7 +473,7 @@ int main(  int argc, char** argv ) {
 			p2LHand.separateROI (p2RHand, p2.handRad, p2.handRad);
 		}
 
-			// Separate the two players
+		// Separate the two players
 		if (p1.ylHand - p1.handRad >= frame.rows/2) {
 			p2LHand.separatePlayers(frame, p1LHand, p1.handRad, p2.handRad, false);
 			p2RHand.separatePlayers(frame, p1LHand, p1.handRad, p2.handRad, false);
@@ -457,6 +511,7 @@ int main(  int argc, char** argv ) {
 		imshow("Player 1 ROI", frame);
 		imshow("Player 2 ROI", frame2);
 
+		// If neither player runs out of health, show game interface
 		if (not (p1.wBar == 0 || p2.wBar == 0)) {
 					// Visualise players on game window
 				// A black background to erase previous image
@@ -480,6 +535,7 @@ int main(  int argc, char** argv ) {
 			imshow("Boxing Game 2", game2);
 		}
 
+		// If either player runs out of health, show "You Win/Lose" window
 		if (p1.wBar == 0) {
 			winner = player2Str;
 
@@ -510,6 +566,7 @@ int main(  int argc, char** argv ) {
 		}
 	}
 
+	// After the game ends, record winner's name
 	char winnerChar[winner.size() + 1];
 	strcpy(winnerChar, winner.c_str());
 
